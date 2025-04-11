d927d4b6efc3927d06c744b994902d9e91a93f36
carlosmagno02 <carlosmabr44@gmail.com>
2025-04-11 08:35:28 -0300

    feat: updating button sidebar

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,13 +1,25 @@
+import { LayoutGridIcon, PackageIcon, ShoppingBasketIcon } from 'lucide-react'
+import SidebarButton from "./sidebar-button";
+
 const Sidebar = () => {
     return (
         <div className="w-64 bg-white">
             <div className="px-8 py-6">
                 <h1 className="font-bold text-2xl">STOCKLY</h1>
             </div>
-            <div className="flex flex-col gap-2 p2">
-                <button className="px-6 py-3">Dashboard</button>
-                <button className="px-6 py-3">Produtos</button>
-                <button className="px-6 py-3">Vendas</button>
+            <div className="flex flex-col gap-2 p-2">
+                <SidebarButton href="/">
+                    <LayoutGridIcon size={20} />
+                    Dashboard
+                </SidebarButton>
+                <SidebarButton href="/products">
+                    <PackageIcon size={20} />
+                    Produtos
+                </SidebarButton>
+                <SidebarButton href="/sales">
+                    <ShoppingBasketIcon size={20} />
+                    Vendas
+                </SidebarButton>
             </div>
         </div>
     );
